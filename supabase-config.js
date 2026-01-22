d1fd0ba2f86840459f9877560e58d740596d5639
heyi666-a11y <3771269361@qq.com>
2026-01-22 22:30:25 +0800

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,6 +0,0 @@
-// Supabase配置文件
-export const supabaseUrl = 'https://yzhnrtinfztdumfzuxvk.supabase.co';
-export const supabaseKey = 'sb_publishable_yebBr2U45Y5yWMUzNcaBkw_NRaAwZEM';
-
-// 注意：这个文件只在图书馆系统中使用，总系统不依赖它
-// 图书馆系统将通过动态加载的方式使用这个文件
